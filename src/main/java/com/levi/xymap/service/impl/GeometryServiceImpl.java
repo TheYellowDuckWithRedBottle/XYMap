3fd617bc7685059f8280547fa22bffbefd120d38
Levi_Fan <1521681359@qq.com>
2023-03-04 23:14:54 +0800

    添加shpFile的处理

--- src/main/java/com/levi/xymap/service/impl/GeometryServiceImpl.java
+++ src/main/java/com/levi/xymap/service/impl/GeometryServiceImpl.java
@@ -3,13 +3,23 @@ package com.levi.xymap.service.impl;
 import com.levi.xymap.entity.Document;
 import com.levi.xymap.service.DocumentService;
 import com.levi.xymap.service.GeometryService;
+import org.geotools.data.FeatureSource;
+import org.geotools.data.PrjFileReader;
+import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.dbf.DbaseFileReader;
+import org.geotools.data.shapefile.files.ShpFiles;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.NullLiteral;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.channels.ReadableByteChannel;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName
@@ -39,10 +49,35 @@ public class GeometryServiceImpl implements GeometryService {
                   FilePrj = new File(path+document.getName());
               }
           }
+          Map result = parseShpfile(FileShp,FileDbf,FilePrj,null);
 
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+    }
+    private Map parseShpfile(File shpFile,File bdfFile, File prjFile,Map properties){
+        ShapefileDataStore shpFileDataStore = null;
+        FeatureSource featureSource = null;
+        DbaseFileReader reader = null;
+        PrjFileReader projFileReader = null;
+        CoordinateReferenceSystem sourceCrs = null;
+        if(shpFile != null) {
+            String str = shpFile.toString();
+        }
+        if(prjFile !=null) {
+            try {
+                projFileReader = new PrjFileReader((ReadableByteChannel) new ShpFiles(prjFile));
+                CoordinateReferenceSystem crs = projFileReader.getCoordinateReferenceSystem();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (FactoryException e) {
+                e.printStackTrace();
+            } finally {
+                projFileReader.close();
+            }
+
         }
 
     }
